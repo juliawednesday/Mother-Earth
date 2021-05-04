eac5ad103fb3a2340200d2da06e4f6eab955cd2d
juliawednesday <yulia.sereda@outlook.com>
2021-05-04 22:01:47 +0300

    Adaptation final

--- js/main.js
+++ js/main.js
@@ -58,6 +58,13 @@ close.addEventListener('click', () => {
 		});
 	});
 
+	//InputMask //
+
+	let selector = document.querySelectorAll('input[type=tel]');
+
+	let im = new Inputmask ('+7 (999) 999-99-99');
+
+	im.mask(selector);
 
 // validate forms
 let validateForms = function(selector, rules) {
